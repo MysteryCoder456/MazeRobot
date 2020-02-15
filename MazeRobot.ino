5c04d6139d0da386a4588ee453e3adf4f0076b72
MysteryCoder456 <rehatbir.singh@gmail.com>
2020-02-15 16:36:57 +0400

    Added Logic

--- MazeRobot.ino
+++ MazeRobot.ino
@@ -1,6 +1,5 @@
 #include "SonicSensor.h"
 #include "Car.h"
-#include "Car.h"
 
 SonicSensor leftSensor = SonicSensor(2, 3);
 SonicSensor frontSensor = SonicSensor(4, 5);
@@ -12,14 +11,47 @@ void setup()
 {
     Serial.begin(9600);
     Serial.println("Started!");
-
-    pinMode(13, OUTPUT);
 }
 
 void loop()
 {
-//    delay(100);
-    Serial.print((String)leftSensor.getDistance() + " ");
-    Serial.print((String)frontSensor.getDistance() + " ");
-    Serial.println(rightSensor.getDistance());
+    long left = leftSensor.getDistance();
+    long front = frontSensor.getDistance();
+    long right = rightSensor.getDistance();
+
+    long frontThreshold = 10;
+    long sideThreshold = 15;
+
+    int moveDelay = 1000;
+    int turnDelay = 500;
+
+    // Not Blocked Left
+    if (left >= sideThreshold)
+    {
+        robot.turnLeft();
+        delay(turnDelay);
+        
+        robot.moveForward();
+        delay(moveDelay);
+    }
+    // Not Blocked Right
+    else if (right >= sideThreshold)
+    {
+        robot.turnRight();
+        delay(turnDelay);
+        
+        robot.moveForward();
+        delay(moveDelay);
+    }
+    // Not Blocked Front
+    else if (front >= frontThreshold)
+    {
+        robot.moveForward();
+    }
+    // Blocked Front, Left and Right
+    else if (front < frontThreshold && left < sideThreshold && right < sideThreshold)
+    {
+        robot.turnLeft():
+        delay(turnDelay * 2);
+    }
 }
